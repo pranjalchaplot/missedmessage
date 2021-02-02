59a36ea953ebf26854796d37c50e085ec142e2cf
= <=>
2021-02-02 22:11:34 +0530

    initialized join and room components

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+
     return (
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className = "heading">Join</h1>
+                <div><input placeholder= "Name" type="text" className="joinInput" onChange={(e) => setName(e.target.value)}/> </div>
+                <div><input placeholder= "Room" type="text" className="joinInput mt-20" onChange={(e) => setRoom(e.target.value)}/> </div>
+                <Link to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+        </div>
     )
 };
 
