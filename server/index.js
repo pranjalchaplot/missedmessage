4156e0d1a034227f83161eea3f32e524dc22686c
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-02-08 00:13:15 +0530

    implement user join protocols

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
+const { addUser, getUser, removeUser, getUserInRoom} = require( './users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -19,11 +21,18 @@ app.use(cors());
 app.use(router); 
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection');
-
-    socket.on('join', ({name, room}, 
+    socket.on('join', ({name, room}, callback
          ) => {
-        console.log(name, room); 
+        const { error, user } = addUser({id: socket.id, name, room});
+
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!!`});
+
+        socket.join(user.room);
+
+        callback();
     });
 
     socket.on('disconnect', () => {
