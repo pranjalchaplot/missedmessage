85f14f9e28215f4c6d60d32a08ed505a43d2e576
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-02-10 14:23:16 +0530

    front end support

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ( { location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://missedmessage.herokuapp.com/';
 
     
 
